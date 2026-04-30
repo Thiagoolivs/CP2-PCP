291d124c2ce6cad963be7960d70b4979aca0c706
bielw12 <gabriel.pedsouz@gmail.com>
2026-04-30 19:25:30 -0300

    terminado

--- pcp_1sem_cp2_ex02.py
+++ pcp_1sem_cp2_ex02.py
@@ -2,9 +2,30 @@ a = float(input("Insira o lado A (sendo o maior lado) do triângulo: "))
 b = float(input("Insira o lado B do triângulo: "))
 c = float(input("Insira o lado C do triângulo: "))
 
-if a >= b and c:
-    print('nao forma triangulo')
-elif a**2 == b**2 and c**2:
-    print('triangulo retangulo')
-elif a**2 > b**2 and c**2
-    print("t")
+#se forma ou não triângulo
+if not (a + b > c and a + c > b and b + c > a):
+    print('não forma triangulo')
+
+#lados
+if a == b and b == c:
+    print('triângulo equilátero')
+elif a==b or a == c or b ==c:
+    ('triângulo isósceles')
+elif a != b or a !=c or b != c:
+    print('triangulo escaleno')
+else:
+    print('Algum erro de digitação, revise.')
+
+#angulos
+hip = a**2
+cat = b**2 + c**2
+
+#definição do tipo
+if hip == cat:
+    print('triângulo retângulo')
+elif hip > cat:
+    print('triângulo obtuso')
+elif hip < cat:
+    print('triângulo agudo')
+else:
+    print('existe algum erro de digitação, revise.')
--- pcp_1sem_cp2_ex03.py
+++ pcp_1sem_cp2_ex03.py
@@ -1,16 +1,40 @@
-import pandas as pd
+#Exercício 2 - notas do semestre e média
+cp1 = cp1 = float(input("Digite a nota do checkpoint 1: "))
+while cp1 < 0 or cp1 > 10:
+    cp1 = float(input("Digite a nota do checkpoint 1: "))
 
-cp = list(map(float, input("Digite as notas dos 3 checkpoints separados por espaço.").split()))
+cp2 = float(input("Digite a nota do checkpoint 2: "))
+while cp2 < 0 or cp2 > 10:
+    cp2 = float(input("Digite a nota do checkpoint 1: "))
+
+cp3 = float(input("Digite a nota do checkpoint 3: "))
+while cp3 < 0 or cp3 > 10:
+    cp3 = float(input("Digite a nota do checkpoint 1: "))
+
 sp1 = float(input("Digite a nota da sprint 1: "))
-sp2 = float(input("Digite a nota da sprint 2: "))
-gs = float(input("Digite sua nota Global Solution: "))
+while sp1 < 0 or sp1 > 10:
+    sp1 = float(input("Digite a nota do checkpoint 1: "))
 
-#ordena em ordem crescente e remove o menor valor(o primeiro item)
-cp.sort()
-cp.pop(0)
+sp2 = float(input("Digite a nota da sprint 2: "))
+while sp2 < 0 or sp2 > 10:
+    sp2 = float(input("Digite a nota do checkpoint 1: "))
+
+gs = float(input("Digite sua nota Global Solution: "))
+while gs < 0 or gs > 10:
+    gs = float(input("Digite a nota do checkpoint 1: "))
+
+#definindo qual a menor nota entre os checkpoint
+if cp1 <= cp2 and cp1 <= cp3:
+    mCP = cp1
+elif cp2 <= cp1 and cp2 <= cp3:
+    mCP = cp2
+else:
+    mCP = cp3
+
+Checkpoints = cp1 + cp2 + cp3 - mCP
 
 #calculo da media sem peso
-mediaSpeso = (cp[0] + cp[1] + sp1 + sp2)/4
+mediaSpeso = (Checkpoints + sp1 + sp2) / 4
 
 #calculo da media com peso
 mediaCpeso = mediaSpeso * 0.4 + (gs * 0.6)
@@ -20,10 +44,3 @@ print()
 print('---Seus resultados do semestre---')
 print(f'A sua média do semestre sem peso é de: {mediaSpeso:.1f}')
 print(f'A sua média do semestre com peso é de: {mediaCpeso:.1f}')
-
-#criação de uma tabela para mostrar as notas, usando pandas
-tabela = pd.DataFrame ({
-    'Média-S-Peso' , mediaSpeso,
-    'Média-C-peso' , mediaCpeso
-})
-print(tabela)
